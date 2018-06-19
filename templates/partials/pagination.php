b180c65a7bf81f04b5b1fa33b61f03b42cb7c304
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2018-06-19 15:38:16 +0200

    Fix non-English links in `pagination` template partial

--- templates/partials/pagination.php
+++ templates/partials/pagination.php
@@ -5,22 +5,22 @@
 <?php
                 if ($pagination->hasPreviousPage()):
 ?>
-                <a class="pagination-previous" href="<?= $pagination->previousPageUri() ?>">&larr; Precedente</a>
+                <a class="pagination-previous" href="<?= $pagination->previousPageUri() ?>">&larr; Previous</a>
 <?php
                 else:
 ?>
-                <a class="pagination-previous disabled">&larr; Precedente</a>
+                <a class="pagination-previous disabled">&larr; Previous</a>
 <?php
                 endif;
 ?>
 <?php
                 if ($pagination->hasNextPage()):
 ?>
-                <a class="pagination-next" href="<?= $pagination->nextPageUri() ?>">Successiva &rarr;</a>
+                <a class="pagination-next" href="<?= $pagination->nextPageUri() ?>">Next &rarr;</a>
 <?php
                 else:
 ?>
-                <a class="pagination-next disabled">Successiva &rarr;</a>
+                <a class="pagination-next disabled">Next &rarr;</a>
 <?php
                 endif;
 ?>
