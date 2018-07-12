f12945877002fa2fba55df9bef5c4b41b6b47213
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2018-07-13 01:29:11 +0200

    Add type declarations in `Pages` and `Users` controllers

--- admin/src/Controllers/Users.php
+++ admin/src/Controllers/Users.php
@@ -178,7 +178,7 @@ class Users extends AbstractController
         ));
     }
 
-    protected function deleteAvatar($user)
+    protected function deleteAvatar(User $user)
     {
         $avatar = $user->avatar()->path();
         if (FileSystem::exists($avatar)) {
