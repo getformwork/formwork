eb431fa9da23ed3ba28dc40fe5bf9c88e99c8de9
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2018-07-15 17:40:59 +0200

    Add `parsers.use_php_yaml` option to decide PHP Yaml extension behavior

--- formwork/Parsers/YAML.php
+++ formwork/Parsers/YAML.php
@@ -2,14 +2,17 @@
 
 namespace Formwork\Parsers;
 
+use Formwork\Core\Formwork;
 use Formwork\Utils\FileSystem;
 use Spyc;
 
 class YAML
 {
+    public static $PHPYAMLmode;
+
     public static function parse($input)
     {
-        if (function_exists('yaml_parse')) {
+        if (function_exists('yaml_parse') && static::PHPYAMLmode('parse')) {
             if (!preg_match('/^---\n/', $input)) {
                 $input = "---\n" . $input;
             }
@@ -25,12 +28,37 @@ class YAML
 
     public static function encode($data)
     {
-        if (function_exists('yaml_emit')) {
+        if (function_exists('yaml_emit') && static::PHPYAMLmode('emit')) {
             if (empty($data)) {
                 return '';
             }
             return preg_replace('/^---[\n ]|\n\.{3}$/', '', yaml_emit((array) $data));
         }
         return Spyc::YAMLDump($data, false, 0, true);
+    }
+
+    protected static function PHPYAMLmode($pattern)
+    {
+        if (is_null(static::$PHPYAMLmode)) {
+            $option = Formwork::instance()->option('parsers.use_php_yaml');
+            if ($option) {
+                switch (strtolower($option)) {
+                    case 'all':
+                        static::$PHPYAMLmode = 'all';
+                        break;
+                    case 'emit':
+                        static::$PHPYAMLmode = 'emit';
+                        break;
+                    case 'parse':
+                        static::$PHPYAMLmode = 'parse';
+                        break;
+                    case 'none':
+                    default:
+                        static::$PHPYAMLmode = false;
+                        break;
+                }
+            }
+        }
+        return static::$PHPYAMLmode === $pattern || static::$PHPYAMLmode == 'all';
     }
 }
