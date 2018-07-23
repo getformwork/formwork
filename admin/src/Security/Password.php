a3a8476f8dbdc9b1f1de8dcf38758c3428923087
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2018-07-23 20:31:26 +0200

    Fix unintended non-static method `Password::verify()`

--- admin/src/Security/Password.php
+++ admin/src/Security/Password.php
@@ -9,7 +9,7 @@ class Password
         return password_hash($password, PASSWORD_DEFAULT);
     }
 
-    public function verify($password, $hash)
+    public static function verify($password, $hash)
     {
         return password_verify($password, $hash);
     }
