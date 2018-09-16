f3428966c87a130e4d5e27fab8111834daac9d98
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2018-09-16 21:25:48 +0200

    Allow language strings in schemes

---
+++ admin/src/Fields/Translator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Formwork\Admin\Fields;
+
+use Formwork\Admin\Utils\Language;
+
+class Translator
+{
+    const INTERPOLATION_REGEX = '/^{{([\-._a-z]+)}}$/i';
+
+    protected static $ignore = array('name', 'type', 'import', 'fields');
+
+    public static function translate(Field $field)
+    {
+        $language = Language::language();
+        foreach ($field->toArray() as $key => $value) {
+            if (static::isTranslatable($key, $field)) {
+                if (is_array($value)) {
+                    if (isset($value[$language])) {
+                        $value = $value[$language];
+                    }
+                } elseif (!is_string($value)) {
+                    continue;
+                }
+                $field->set($key, static::interpolate($value));
+            }
+        }
+    }
+
+    protected static function isTranslatable($key, Field $field)
+    {
+        if (in_array($key, static::$ignore)) {
+            return false;
+        }
+        $translate = $field->get('translate', true);
+        if (is_array($translate)) {
+            return in_array($key, $translate);
+        }
+        return $translate;
+    }
+
+    protected static function interpolate($value)
+    {
+        if (is_array($value)) {
+            return array_map(array(static::class, 'interpolate'), $value);
+        }
+        if (is_string($value) && (bool) preg_match(self::INTERPOLATION_REGEX, $value, $matches)) {
+            return Language::get($matches[1]);
+        }
+        return $value;
+    }
+}
