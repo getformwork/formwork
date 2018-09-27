e777a365934cb9f391fd98781476f7b38a124791
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2018-09-27 13:05:04 +0200

    Use `LocalizedException` in `Backupper` class and `Backup` controller

--- admin/src/Utils/ZipErrors.php
+++ admin/src/Utils/ZipErrors.php
@@ -6,28 +6,55 @@ use ZipArchive;
 
 class ZipErrors
 {
-    public static function getMessage($code)
-    {
-        switch ($code) {
-            case ZipArchive::ER_EXISTS:
-                return Language::get('zip.error.already-exists');
-            case ZipArchive::ER_INCONS:
-                return Language::get('zip.error.inconsistent-archive');
-            case ZipArchive::ER_INVAL:
-                return Language::get('zip.error.invalid-argument');
-            case ZipArchive::ER_MEMORY:
-                return Language::get('zip.error.memory-failure');
-            case ZipArchive::ER_NOENT:
-                return Language::get('zip.error.not-found');
-            case ZipArchive::ER_NOZIP:
-                return Language::get('zip.error.not-zip-archive');
-            case ZipArchive::ER_OPEN:
-                return Language::get('zip.error.cannot-open');
-            case ZipArchive::ER_READ:
-            case ZipArchive::ER_SEEK:
-                return Language::get('zip.error.cannot-read');
-            default:
-                return Language::get('zip.error.unspecified');
-        }
-    }
+    public static $errorMessages = array(
+        ZipArchive::ER_MULTIDISK   => 'Multi-disk zip archives not supported',
+        ZipArchive::ER_RENAME      => 'Renaming temporary file failed',
+        ZipArchive::ER_CLOSE       => 'Closing zip archive failed',
+        ZipArchive::ER_SEEK        => 'Seek error',
+        ZipArchive::ER_READ        => 'Read error',
+        ZipArchive::ER_WRITE       => 'Write error',
+        ZipArchive::ER_CRC         => 'CRC error',
+        ZipArchive::ER_ZIPCLOSED   => 'Containing zip archive was closed',
+        ZipArchive::ER_NOENT       => 'No such file',
+        ZipArchive::ER_EXISTS      => 'File already exists',
+        ZipArchive::ER_OPEN        => 'Cannot open file',
+        ZipArchive::ER_TMPOPEN     => 'Failure to create temporary file',
+        ZipArchive::ER_ZLIB        => 'Zlib error',
+        ZipArchive::ER_MEMORY      => 'Memory allocation failure',
+        ZipArchive::ER_CHANGED     => 'Entry has been changed',
+        ZipArchive::ER_COMPNOTSUPP => 'Compression method not supported',
+        ZipArchive::ER_EOF         => 'Premature EOF',
+        ZipArchive::ER_INVAL       => 'Invalid argument',
+        ZipArchive::ER_NOZIP       => 'Not a zip archive',
+        ZipArchive::ER_INTERNAL    => 'Internal error',
+        ZipArchive::ER_INCONS      => 'Zip archive inconsistent',
+        ZipArchive::ER_REMOVE      => 'Cannot remove file',
+        ZipArchive::ER_DELETED     => 'Entry has been deleted'
+    );
+
+    public static $errorLanguageStrings = array(
+        ZipArchive::ER_MULTIDISK   => 'zip.error.unspecified',
+        ZipArchive::ER_RENAME      => 'zip.error.unspecified',
+        ZipArchive::ER_CLOSE       => 'zip.error.unspecified',
+        ZipArchive::ER_SEEK        => 'zip.error.cannot-read',
+        ZipArchive::ER_READ        => 'zip.error.cannot-read',
+        ZipArchive::ER_WRITE       => 'zip.error.unspecified',
+        ZipArchive::ER_CRC         => 'zip.error.unspecified',
+        ZipArchive::ER_ZIPCLOSED   => 'zip.error.unspecified',
+        ZipArchive::ER_NOENT       => 'zip.error.not-found',
+        ZipArchive::ER_EXISTS      => 'zip.error.already-exists',
+        ZipArchive::ER_OPEN        => 'zip.error.cannot-open',
+        ZipArchive::ER_TMPOPEN     => 'zip.error.unspecified',
+        ZipArchive::ER_ZLIB        => 'zip.error.unspecified',
+        ZipArchive::ER_MEMORY      => 'zip.error.memory-failure',
+        ZipArchive::ER_CHANGED     => 'zip.error.unspecified',
+        ZipArchive::ER_COMPNOTSUPP => 'zip.error.unspecified',
+        ZipArchive::ER_EOF         => 'zip.error.unspecified',
+        ZipArchive::ER_INVAL       => 'zip.error.invalid-argument',
+        ZipArchive::ER_NOZIP       => 'zip.error.not-zip-archive',
+        ZipArchive::ER_INTERNAL    => 'zip.error.unspecified',
+        ZipArchive::ER_INCONS      => 'zip.error.inconsistent-archive',
+        ZipArchive::ER_REMOVE      => 'zip.error.unspecified',
+        ZipArchive::ER_DELETED     => 'zip.error.unspecified'
+    );
 }
