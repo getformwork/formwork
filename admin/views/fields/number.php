56f09c42b3f3dd5cbb1302d6c58ac0b5a09d4772
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2018-10-09 23:20:03 +0200

    Add `required` and `disabled` attributes support to fields

--- admin/views/fields/number.php
+++ admin/views/fields/number.php
@@ -1,2 +1,2 @@
 <?php if ($field->has('label')): ?><label for="<?= $field->name() ?>"><?= $field->label() ?></label><?php endif; ?>
-<input type="number" id="<?= $field->name() ?>" name="<?= $field->name() ?>" min="<?= $field->get('min') ?>" max="<?= $field->get('max') ?>" step="<?= $field->get('step') ?>" value="<?= $field->value() ?>"<?= $field->get('required') ? ' required' : '' ?>>
+<input type="number" id="<?= $field->name() ?>" name="<?= $field->name() ?>" min="<?= $field->get('min') ?>" max="<?= $field->get('max') ?>" step="<?= $field->get('step') ?>" value="<?= $field->value() ?>"<?php if ($field->get('required')): ?> required<?php endif; ?><?php if ($field->get('disabled')): ?> disabled<?php endif; ?>>
--- admin/views/fields/textarea.php
+++ admin/views/fields/textarea.php
@@ -1,2 +1,2 @@
 <?php if ($field->has('label')): ?><label for="<?= $field->name() ?>"><?= $field->label() ?></label><?php endif; ?>
-<textarea id="<?= $field->name() ?>" name="<?= $field->name() ?>"<?php if ($field->get('required')): ?> required<?php endif; ?>><?= $field->value() ?></textarea>
+<textarea id="<?= $field->name() ?>" name="<?= $field->name() ?>"<?php if ($field->get('required')): ?> required<?php endif; ?><?php if ($field->get('disabled')): ?> disabled<?php endif; ?>><?= $field->value() ?></textarea>
