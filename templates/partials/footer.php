f699c62cb15d0c0c3c6de93df851895979bf6e6c
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-02-28 17:55:23 +0100

    Bump year in default theme footer

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,6 +1,6 @@
     <footer>
         <div class="container small">
-            &copy; 2017-2018 &mdash; Made with <a href="http://github.com/giuscris/formwork">Formwork</a>
+            &copy; 2017-2019 &mdash; Made with <a href="http://github.com/giuscris/formwork">Formwork</a>
         </div>
     </footer>
 </body>
