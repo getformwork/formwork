8ce9afc6e2e38c16cd6b4e841e77ca0a7f188786
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-03-04 19:41:06 +0100

    Add Session timeout

--- admin/src/Utils/Session.php
+++ admin/src/Utils/Session.php
@@ -2,6 +2,7 @@
 
 namespace Formwork\Admin\Utils;
 
+use Formwork\Core\Formwork;
 use Formwork\Utils\HTTPRequest;
 use Formwork\Utils\Cookie;
 
@@ -15,20 +16,24 @@ class Session
         if (session_status() === PHP_SESSION_NONE) {
             ini_set('session.use_strict_mode', true);
             $options = array(
+                'expires'  => 0,
                 'path'     => HTTPRequest::root(),
                 'secure'   => HTTPRequest::isHTTPS(),
                 'httponly' => true,
                 'samesite' => Cookie::SAMESITE_STRICT
             );
+            if (($timeout = Formwork::instance()->option('admin.session_timeout')) > 0) {
+                $options['expires'] = time() + $timeout * 60;
+            }
             session_name('formwork_session');
             session_start();
-            if (!isset($_COOKIE[session_name()])) {
-                // Send session cookie if not already sent
+            if (!isset($_COOKIE[session_name()]) || $options['expires'] > 0) {
+                // Send session cookie if not already sent or timeout is set
                 Cookie::send(session_name(), session_id(), $options, true);
             } elseif ($_COOKIE[session_name()] !== session_id()) {
                 // Remove cookie if session id is not valid
                 unset($_COOKIE[session_name()]);
-                Cookie::send(session_name(), '', $options + array('expires' => time() - 3600), true);
+                Cookie::send(session_name(), '', array('expires' => time() - 3600) + $options, true);
             }
         }
     }
