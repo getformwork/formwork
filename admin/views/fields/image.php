5b493b3be0a68f865bf6a80037d6654f6f52db3e
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-03-17 22:01:59 +0100

    Add input reset button to `image` field

--- admin/views/fields/image.php
+++ admin/views/fields/image.php
@@ -1,2 +1,5 @@
 <?php if ($field->has('label')): ?><label for="<?= $field->name() ?>"><?= $field->label() ?></label><?php endif; ?>
-<input class="image-input" type="text" id="<?= $field->name() ?>" name="<?= $field->name() ?>" value="<?= basename($field->value()) ?>" placeholder="<?= $field->placeholder() ?>" readonly>
+<div class="input-wrap">
+    <input class="image-input" type="text" id="<?= $field->name() ?>" name="<?= $field->name() ?>" value="<?= basename($field->value()) ?>" placeholder="<?= $field->placeholder() ?>" readonly>
+    <span class="input-reset" data-reset="<?= $field->name() ?>"><i class="i-times-circle"></i></span>
+</div>
