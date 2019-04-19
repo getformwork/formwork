c25e565098fdc345015ba153d7dac1e4bee99d65
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-04-19 18:49:05 +0200

    Add dropdowns component

--- admin/assets/js/app.js
+++ admin/assets/js/app.js
@@ -2,6 +2,7 @@ var Formwork = {
     init: function () {
         Formwork.Modals.init();
         Formwork.Forms.init();
+        Formwork.Dropdowns.init();
         Formwork.Tooltips.init();
 
         Formwork.Dashboard.init();
@@ -120,6 +121,25 @@ Formwork.Dashboard = {
                 }, 1000);
             });
         });
+    }
+};
+
+Formwork.Dropdowns = {
+    init: function () {
+        if ($('.dropdown').length > 0) {
+            $(document).click(function (event) {
+                var $button = $(event.target).closest('.dropdown-button');
+                if ($button.length > 0) {
+                    var $dropdown = $('#' + $button.attr('data-dropdown'), '.dropdown');
+                    var isVisible = $dropdown.is(':visible');
+                    event.preventDefault();
+                }
+                $('.dropdown-menu').hide();
+                if ($dropdown !== undefined && !isVisible) {
+                    $dropdown.show();
+                }
+            });
+        }
     }
 };
 
---
+++ admin/assets/js/src/components/dropdowns.js
@@ -0,0 +1,18 @@
+Formwork.Dropdowns = {
+    init: function () {
+        if ($('.dropdown').length > 0) {
+            $(document).click(function (event) {
+                var $button = $(event.target).closest('.dropdown-button');
+                if ($button.length > 0) {
+                    var $dropdown = $('#' + $button.attr('data-dropdown'), '.dropdown');
+                    var isVisible = $dropdown.is(':visible');
+                    event.preventDefault();
+                }
+                $('.dropdown-menu').hide();
+                if ($dropdown !== undefined && !isVisible) {
+                    $dropdown.show();
+                }
+            });
+        }
+    }
+};
