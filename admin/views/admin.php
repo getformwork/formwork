6b3acf856d7e8ccad81dd2299ed089898fed49b9
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-04-26 14:20:56 +0200

    Update jQuery version

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -19,7 +19,7 @@
 ?>
     <link rel="shortcut icon" href="<?= $this->assets()->uri('images/icon.png') ?>">
     <link rel="stylesheet" href="<?= $this->assets()->uri('css/admin.min.css', true) ?>">
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
     <script src="<?= $this->assets()->uri('js/app.min.js', true) ?>"></script>
     <script src="<?= $this->assets()->uri('js/vendor.min.js', true) ?>"></script>
 </head>
