59cdb759c3b28c30ad232af459e754a00d2851c7
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-04-28 19:36:42 +0200

    Fix `menu` partial template overwriting `$page` variable

--- templates/partials/menu.php
+++ templates/partials/menu.php
@@ -3,8 +3,8 @@
         <a class="menu-header" href="<?= $site->uri() ?>"><?= $site->title() ?></a>
         <button type="button" class="menu-toggle" data-toggle="main-menu" aria-expanded="false">&#9776;</button>
         <div class="menu-list menu-collapse" id="main-menu">
-        <?php foreach ($site->children()->filter('visible') as $page): ?>
-            <a class="menu-item<?php if ($page->isCurrent()): ?> active<?php endif; ?>" href="<?= $page->uri() ?>"><?= $page->get('menu', $page->title()) ?></a>
+        <?php foreach ($site->children()->filter('visible') as $item): ?>
+            <a class="menu-item<?php if ($item->isCurrent()): ?> active<?php endif; ?>" href="<?= $item->uri() ?>"><?= $item->get('menu', $item->title()) ?></a>
         <?php endforeach; ?>
         </div>
     </div>
