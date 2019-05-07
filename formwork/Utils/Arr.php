0215231b4577ba5202d67732ff1cca6b41d18a5f
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-05-07 16:53:34 +0200

    Add `Arr` class

---
+++ formwork/Utils/Arr.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Formwork\Utils;
+
+class Arr
+{
+    /**
+     * Get data by key returning a default value if key is not present in a given array,
+     * using dot notation to traverse if literal key is not found
+     *
+     * @param array      $array
+     * @param string     $key
+     * @param mixed|null $default
+     *
+     * @return mixed|null
+     */
+    public static function get($array, $key, $default = null)
+    {
+        if (array_key_exists($key, $array)) {
+            return $array[$key];
+        }
+        foreach (explode('.', $key) as $segment) {
+            if (!is_array($array) || !array_key_exists($segment, $array)) {
+                return $default;
+            }
+            $array = $array[$segment];
+        }
+        return $array;
+    }
+
+    /**
+     * Return whether a key is present in a given array, using dot notation to traverse
+     * if literal key is not found
+     *
+     * @param array  $array
+     * @param string $key
+     *
+     * @return bool
+     */
+    public static function has($array, $key)
+    {
+        if (array_key_exists($key, $array)) {
+            return true;
+        }
+        foreach (explode('.', $key) as $segment) {
+            if (!is_array($array) || !array_key_exists($segment, $array)) {
+                return false;
+            }
+            $array = $array[$segment];
+        }
+        return true;
+    }
+}
