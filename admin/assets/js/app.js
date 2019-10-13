20168c0f7a5ee89e49ce0a2ca42818080427d960
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-10-13 14:52:51 +0200

    Add support for options dropdown list to `tags` field

--- admin/assets/js/app.js
+++ admin/assets/js/app.js
@@ -1473,10 +1473,12 @@ Formwork.Utils = {
 (function ($) {
     $.fn.tagInput = function () {
         this.each(function () {
+            var options = {addKeyCodes: [32]};
             var $this = $(this);
             var $field = createField();
             var $target = $('.tag-hidden-input', $field);
             var $input = $('.tag-inner-input', $field);
+            var $dropdown = createDropdown();
             var tags = [];
 
             function createField() {
@@ -1490,6 +1492,21 @@ Formwork.Utils = {
                 );
                 $this.replaceWith(field);
                 return field;
+            }
+
+            function createDropdown() {
+                if ($this.is('[data-options]')) {
+                    var list = $.parseJSON($this.attr('data-options'));
+                    var dropdown = $('<div class="dropdown-list"></div>');
+                    $field.append(dropdown);
+                    for (var key in list) {
+                        dropdown.append(
+                            '<div class="dropdown-item" data-value="' + key + '">' + list[key] + '</div>'
+                        );
+                    }
+                    return dropdown;
+                }
+                return null;
             }
 
             function updateTags() {
@@ -1510,17 +1527,32 @@ Formwork.Utils = {
                 }
             }
 
+            function validateTag(value) {
+                if ($input.data('tags').indexOf(value) === -1) {
+                    if ($dropdown !== null) {
+                        return $('[data-value="' + value + '"]', $dropdown).length > 0;
+                    }
+                    return true;
+                }
+                return false;
+            }
+
             function insertTag(value) {
                 $input.before('\n<span class="tag">' + value + '<i class="tag-remove"></i></span>');
             }
 
             function addTag(value) {
-                if ($input.data('tags').indexOf(value) === -1) {
+                if (validateTag(value)) {
                     $input.data('tags').push(value);
                     insertTag(value);
                     updateTags();
+                } else {
+                    updatePlaceholder();
                 }
                 $input.val('');
+                if ($dropdown !== null) {
+                    updateDropdown();
+                }
             }
 
             function removeTag(value) {
@@ -1530,6 +1562,97 @@ Formwork.Utils = {
                     tags.splice(index, 1);
                     $input.data('tags', tags);
                     updateTags();
+                }
+                if ($dropdown !== null) {
+                    updateDropdown();
+                }
+            }
+
+            function clearInput() {
+                $input.val('');
+                updatePlaceholder();
+            }
+
+            function updateDropdown() {
+                var tags = $input.data('tags');
+                $('.dropdown-item', $dropdown).each(function () {
+                    var visible = tags.indexOf($(this).attr('data-value')) === -1;
+                    $(this).toggle(visible);
+                });
+                $('.dropdown-item', $dropdown).removeClass('selected');
+                $dropdown.toggle($('.dropdown-item', $dropdown).filter(':visible').length > 0);
+            }
+
+            function filterDropdown(value) {
+                $dropdown.show();
+                $('.dropdown-item', $dropdown).each(function () {
+                    var $this = $(this);
+                    var regexp = new RegExp(Formwork.Utils.escapeRegExp(value), 'i');
+                    var matched = !!$(this).text().match(regexp);
+                    if ($this.is(':visible')) {
+                        $this.toggle(matched);
+                    }
+                });
+                $dropdown.toggle($('.dropdown-item', $dropdown).filter(':visible').length > 0);
+            }
+
+            function scrollToDropdownItem($item) {
+                var dropdownScrollTop = $dropdown.scrollTop();
+                var dropdownTop = $dropdown.position().top;
+                var dropdownHeight = $dropdown.outerHeight();
+                var dropdownBottom = dropdownTop + dropdownHeight;
+                var itemTop = $item.position().top;
+                var itemHeight = $item.outerHeight();
+                var itemBottom = itemTop + itemHeight;
+                if (dropdownBottom - itemBottom > 0 && dropdownBottom - itemBottom < itemHeight) {
+                    $dropdown.scrollTop(dropdownScrollTop + itemHeight);
+                } else if (itemBottom > dropdownHeight || itemBottom < dropdownTop) {
+                    $dropdown.scrollTop(dropdownScrollTop + itemTop);
+                }
+            }
+
+            function addTagFromSelectedDropdownItem() {
+                var $selectedItem = $('.dropdown-item', $dropdown).filter('.selected:visible');
+                if ($selectedItem.length > 0) {
+                    $input.val($selectedItem.attr('data-value'));
+                }
+            }
+
+            function getDropdownItems() {
+                return $('.dropdown-item', $dropdown).filter(':visible');
+            }
+
+            function selectDropdownItem($item) {
+                $('.dropdown-item', $dropdown).removeClass('selected');
+                if ($item.length > 0) {
+                    $item.addClass('selected');
+                    scrollToDropdownItem($item);
+                }
+            }
+
+            function selectFirstDropdownItem() {
+                selectDropdownItem(getDropdownItems().first());
+            }
+
+            function selectLastDropdownItem() {
+                selectDropdownItem(getDropdownItems().last());
+            }
+
+            function selectPrevDropdownItem() {
+                var $prev = getDropdownItems().filter('.selected').prevAll(':visible').first();
+                if ($prev.length > 0) {
+                    selectDropdownItem($prev);
+                } else {
+                    selectLastDropdownItem();
+                }
+            }
+
+            function selectNextDropdownItem() {
+                var $next = getDropdownItems().filter('.selected').nextAll(':visible').first();
+                if ($next.length > 0) {
+                    selectDropdownItem($next);
+                } else {
+                    selectFirstDropdownItem();
                 }
             }
 
@@ -1563,6 +1686,68 @@ Formwork.Utils = {
                 return false;
             });
 
+            if ($dropdown !== null) {
+                $input.on('focus', function () {
+                    if (!$dropdown.is(':visible')) {
+                        updateDropdown();
+                        $dropdown.show();
+                    }
+                }).on('blur', function () {
+                    if ($dropdown.is(':visible')) {
+                        $dropdown.hide();
+                    }
+                }).on('keydown', function (event) {
+                    switch (event.which) {
+                    case 8: // backspace
+                        updateDropdown();
+                        break;
+                    case 13: // enter
+                        if ($dropdown.is(':visible')) {
+                            addTagFromSelectedDropdownItem();
+                            return false;
+                        }
+                        break;
+                    case 38: // up arrow
+                        if ($dropdown.is(':visible')) {
+                            selectPrevDropdownItem();
+                            return false;
+                        }
+                        break;
+                    case 40: // down arrow
+                        if ($dropdown.is(':visible')) {
+                            selectNextDropdownItem();
+                            return false;
+                        }
+                        break;
+                    default:
+                        if (options.addKeyCodes.indexOf(event.which) > -1) {
+                            addTagFromSelectedDropdownItem();
+                            return false;
+                        }
+                    }
+                }).on('keyup', Formwork.Utils.debounce(function (event) {
+                    var value = $input.val().trim();
+                    switch (event.which) {
+                    case 27: // escape
+                        $dropdown.hide();
+                        break;
+                    case 38: // up arrow
+                    case 40: // down arrow
+                        return true;
+                    default:
+                        $dropdown.show();
+                        filterDropdown(value);
+                        if (value.length > 0) {
+                            selectFirstDropdownItem();
+                        }
+                    }
+                }, 100));
+
+                $dropdown.on('click', '.dropdown-item', function () {
+                    addTag($(this).attr('data-value'));
+                });
+            }
+
             $input.on('focus', function () {
                 $field.addClass('focused');
             }).on('blur', function () {
@@ -1572,9 +1757,7 @@ Formwork.Utils = {
                 }
                 $field.removeClass('focused');
             }).on('keydown', function (event) {
-                var options = {addKeyCodes: [32]};
                 var value = $input.val().trim();
-
                 switch (event.which) {
                 case 8: // backspace
                     if (value === '') {
@@ -1589,6 +1772,10 @@ Formwork.Utils = {
                     if (value !== '') {
                         addTag(value);
                     }
+                    return false;
+                case 27: // escape
+                    clearInput();
+                    $input.trigger('blur');
                     return false;
                 default:
                     if (value !== '' && options.addKeyCodes.indexOf(event.which) > -1) {
--- admin/assets/js/src/plugins/taginput.js
+++ admin/assets/js/src/plugins/taginput.js
@@ -1,10 +1,12 @@
 (function ($) {
     $.fn.tagInput = function () {
         this.each(function () {
+            var options = {addKeyCodes: [32]};
             var $this = $(this);
             var $field = createField();
             var $target = $('.tag-hidden-input', $field);
             var $input = $('.tag-inner-input', $field);
+            var $dropdown = createDropdown();
             var tags = [];
 
             function createField() {
@@ -18,6 +20,21 @@
                 );
                 $this.replaceWith(field);
                 return field;
+            }
+
+            function createDropdown() {
+                if ($this.is('[data-options]')) {
+                    var list = $.parseJSON($this.attr('data-options'));
+                    var dropdown = $('<div class="dropdown-list"></div>');
+                    $field.append(dropdown);
+                    for (var key in list) {
+                        dropdown.append(
+                            '<div class="dropdown-item" data-value="' + key + '">' + list[key] + '</div>'
+                        );
+                    }
+                    return dropdown;
+                }
+                return null;
             }
 
             function updateTags() {
@@ -38,17 +55,32 @@
                 }
             }
 
+            function validateTag(value) {
+                if ($input.data('tags').indexOf(value) === -1) {
+                    if ($dropdown !== null) {
+                        return $('[data-value="' + value + '"]', $dropdown).length > 0;
+                    }
+                    return true;
+                }
+                return false;
+            }
+
             function insertTag(value) {
                 $input.before('\n<span class="tag">' + value + '<i class="tag-remove"></i></span>');
             }
 
             function addTag(value) {
-                if ($input.data('tags').indexOf(value) === -1) {
+                if (validateTag(value)) {
                     $input.data('tags').push(value);
                     insertTag(value);
                     updateTags();
+                } else {
+                    updatePlaceholder();
                 }
                 $input.val('');
+                if ($dropdown !== null) {
+                    updateDropdown();
+                }
             }
 
             function removeTag(value) {
@@ -58,6 +90,97 @@
                     tags.splice(index, 1);
                     $input.data('tags', tags);
                     updateTags();
+                }
+                if ($dropdown !== null) {
+                    updateDropdown();
+                }
+            }
+
+            function clearInput() {
+                $input.val('');
+                updatePlaceholder();
+            }
+
+            function updateDropdown() {
+                var tags = $input.data('tags');
+                $('.dropdown-item', $dropdown).each(function () {
+                    var visible = tags.indexOf($(this).attr('data-value')) === -1;
+                    $(this).toggle(visible);
+                });
+                $('.dropdown-item', $dropdown).removeClass('selected');
+                $dropdown.toggle($('.dropdown-item', $dropdown).filter(':visible').length > 0);
+            }
+
+            function filterDropdown(value) {
+                $dropdown.show();
+                $('.dropdown-item', $dropdown).each(function () {
+                    var $this = $(this);
+                    var regexp = new RegExp(Formwork.Utils.escapeRegExp(value), 'i');
+                    var matched = !!$(this).text().match(regexp);
+                    if ($this.is(':visible')) {
+                        $this.toggle(matched);
+                    }
+                });
+                $dropdown.toggle($('.dropdown-item', $dropdown).filter(':visible').length > 0);
+            }
+
+            function scrollToDropdownItem($item) {
+                var dropdownScrollTop = $dropdown.scrollTop();
+                var dropdownTop = $dropdown.position().top;
+                var dropdownHeight = $dropdown.outerHeight();
+                var dropdownBottom = dropdownTop + dropdownHeight;
+                var itemTop = $item.position().top;
+                var itemHeight = $item.outerHeight();
+                var itemBottom = itemTop + itemHeight;
+                if (dropdownBottom - itemBottom > 0 && dropdownBottom - itemBottom < itemHeight) {
+                    $dropdown.scrollTop(dropdownScrollTop + itemHeight);
+                } else if (itemBottom > dropdownHeight || itemBottom < dropdownTop) {
+                    $dropdown.scrollTop(dropdownScrollTop + itemTop);
+                }
+            }
+
+            function addTagFromSelectedDropdownItem() {
+                var $selectedItem = $('.dropdown-item', $dropdown).filter('.selected:visible');
+                if ($selectedItem.length > 0) {
+                    $input.val($selectedItem.attr('data-value'));
+                }
+            }
+
+            function getDropdownItems() {
+                return $('.dropdown-item', $dropdown).filter(':visible');
+            }
+
+            function selectDropdownItem($item) {
+                $('.dropdown-item', $dropdown).removeClass('selected');
+                if ($item.length > 0) {
+                    $item.addClass('selected');
+                    scrollToDropdownItem($item);
+                }
+            }
+
+            function selectFirstDropdownItem() {
+                selectDropdownItem(getDropdownItems().first());
+            }
+
+            function selectLastDropdownItem() {
+                selectDropdownItem(getDropdownItems().last());
+            }
+
+            function selectPrevDropdownItem() {
+                var $prev = getDropdownItems().filter('.selected').prevAll(':visible').first();
+                if ($prev.length > 0) {
+                    selectDropdownItem($prev);
+                } else {
+                    selectLastDropdownItem();
+                }
+            }
+
+            function selectNextDropdownItem() {
+                var $next = getDropdownItems().filter('.selected').nextAll(':visible').first();
+                if ($next.length > 0) {
+                    selectDropdownItem($next);
+                } else {
+                    selectFirstDropdownItem();
                 }
             }
 
@@ -91,6 +214,68 @@
                 return false;
             });
 
+            if ($dropdown !== null) {
+                $input.on('focus', function () {
+                    if (!$dropdown.is(':visible')) {
+                        updateDropdown();
+                        $dropdown.show();
+                    }
+                }).on('blur', function () {
+                    if ($dropdown.is(':visible')) {
+                        $dropdown.hide();
+                    }
+                }).on('keydown', function (event) {
+                    switch (event.which) {
+                    case 8: // backspace
+                        updateDropdown();
+                        break;
+                    case 13: // enter
+                        if ($dropdown.is(':visible')) {
+                            addTagFromSelectedDropdownItem();
+                            return false;
+                        }
+                        break;
+                    case 38: // up arrow
+                        if ($dropdown.is(':visible')) {
+                            selectPrevDropdownItem();
+                            return false;
+                        }
+                        break;
+                    case 40: // down arrow
+                        if ($dropdown.is(':visible')) {
+                            selectNextDropdownItem();
+                            return false;
+                        }
+                        break;
+                    default:
+                        if (options.addKeyCodes.indexOf(event.which) > -1) {
+                            addTagFromSelectedDropdownItem();
+                            return false;
+                        }
+                    }
+                }).on('keyup', Formwork.Utils.debounce(function (event) {
+                    var value = $input.val().trim();
+                    switch (event.which) {
+                    case 27: // escape
+                        $dropdown.hide();
+                        break;
+                    case 38: // up arrow
+                    case 40: // down arrow
+                        return true;
+                    default:
+                        $dropdown.show();
+                        filterDropdown(value);
+                        if (value.length > 0) {
+                            selectFirstDropdownItem();
+                        }
+                    }
+                }, 100));
+
+                $dropdown.on('click', '.dropdown-item', function () {
+                    addTag($(this).attr('data-value'));
+                });
+            }
+
             $input.on('focus', function () {
                 $field.addClass('focused');
             }).on('blur', function () {
@@ -100,9 +285,7 @@
                 }
                 $field.removeClass('focused');
             }).on('keydown', function (event) {
-                var options = {addKeyCodes: [32]};
                 var value = $input.val().trim();
-
                 switch (event.which) {
                 case 8: // backspace
                     if (value === '') {
@@ -117,6 +300,10 @@
                     if (value !== '') {
                         addTag(value);
                     }
+                    return false;
+                case 27: // escape
+                    clearInput();
+                    $input.trigger('blur');
                     return false;
                 default:
                     if (value !== '' && options.addKeyCodes.indexOf(event.which) > -1) {
