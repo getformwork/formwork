aefcc44bd9e99e5d049618aadaef9c1dca8407d0
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-10-16 00:01:27 +0200

    Add `Response` class

---
+++ formwork/Core/Response.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Formwork\Core;
+
+use Formwork\Utils\Header;
+
+class Response
+{
+    /**
+     * Response content
+     *
+     * @var string
+     */
+    protected $content;
+
+    /**
+     * Response HTTP status
+     *
+     * @var int|null
+     */
+    protected $status;
+
+    /**
+     * Response HTTP headers
+     *
+     * @var array
+     */
+    protected $headers;
+
+    /**
+     * Create a new Response instance
+     *
+     * @param string   $content
+     * @param int|null $status
+     * @param array    $headers
+     */
+    public function __construct($content, $status = null, array $headers = array())
+    {
+        $this->content = $content;
+        $this->status = $status;
+        $this->headers = $headers;
+    }
+
+    /**
+     * Return Response content
+     *
+     * @return string
+     */
+    public function content()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Return HTTP status
+     *
+     * @return int
+     */
+    public function status()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Return HTTP headers
+     *
+     * @return array
+     */
+    public function headers()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * Send HTTP status
+     */
+    public function sendStatus()
+    {
+        if (!is_null($this->status)) {
+            Header::status($this->status);
+        }
+    }
+
+    /**
+     * Send HTTP status and headers
+     */
+    public function sendHeaders()
+    {
+        $this->sendStatus();
+
+        if (!empty($this->headers)) {
+            foreach ($this->headers as $fieldName => $fieldValue) {
+                Header::send($fieldName, $fieldValue);
+            }
+        }
+    }
+
+    /**
+     * Send HTTP status, headers and render content
+     */
+    public function render()
+    {
+        $this->sendHeaders();
+        echo $this->content;
+    }
+}
