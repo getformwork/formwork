a7ea2eda75a71b13cdc5b369b6108d6887b4d018
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2019-12-04 01:57:53 +0100

    Use late static binding in `PageCollection::search()`

--- formwork/Core/PageCollection.php
+++ formwork/Core/PageCollection.php
@@ -160,7 +160,7 @@ class PageCollection extends Collection
     {
         $query = trim(preg_replace('/\s+/u', ' ', $query));
         if (strlen($query) < $min) {
-            return new PageCollection();
+            return new static();
         }
 
         $keywords = explode(' ', $query);
