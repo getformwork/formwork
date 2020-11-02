6190b05f569b5afc1a5978c52237cd7139d309a1
Giuseppe Criscione <giuseppepeocriscione@gmail.com>
2020-11-02 22:54:19 +0100

    Remove incompatible parent from `Avatar` class

--- admin/src/Users/Avatar.php
+++ admin/src/Users/Avatar.php
@@ -3,22 +3,62 @@
 namespace Formwork\Admin\Users;
 
 use Formwork\Admin\Admin;
-use Formwork\Files\File;
 use Formwork\Utils\FileSystem;
 
-class Avatar extends File
+class Avatar
 {
+    /**
+     * Default avatar URI
+     *
+     * @var string
+     */
+    protected const DEFAULT_AVATAR_URI = '/assets/images/avatar.png';
+
+    /**
+     * Avatar URI
+     *
+     * @var string
+     */
+    protected $uri;
+
+    /**
+     * Avatar file path
+     *
+     * @var string|null
+     */
+    protected $path;
+
     /**
      * Create a new Avatar instance
      */
     public function __construct(?string $filename)
     {
         $path = ADMIN_PATH . 'avatars/' . $filename;
-        if (!empty($filename) && FileSystem::exists($path)) {
-            parent::__construct($path);
-            $this->uri = Admin::instance()->realUri('/avatars/' . $this->name);
+        if ($filename !== null && FileSystem::exists($path)) {
+            $this->uri = Admin::instance()->realUri('/avatars/' . basename($path));
+            $this->path = $path;
         } else {
-            $this->uri = Admin::instance()->realUri('/assets/images/avatar.png');
+            $this->uri = Admin::instance()->realUri(self::DEFAULT_AVATAR_URI);
         }
+    }
+
+    /**
+     * Return avatar URI
+     *
+     * @return string
+     */
+    public function uri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Return avatar path
+     *
+     * @return string|null
+     */
+    public function path()
+    {
+        return $this->path;
     }
 }
