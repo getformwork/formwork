9702314c64da09c70b7d05b03011ef378e0900df
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2020-12-10 23:02:04 +0100

    Remove superfluous argument

--- formwork/Core/Page.php
+++ formwork/Core/Page.php
@@ -197,7 +197,7 @@ class Page extends AbstractPage
     public function __construct(string $path)
     {
         $this->path = FileSystem::normalizePath($path . DS);
-        $this->relativePath = Str::wrap(Str::removeStart($this->path, Formwork::instance()->site()->path(), DS), DS);
+        $this->relativePath = Str::wrap(Str::removeStart($this->path, Formwork::instance()->site()->path()), DS);
         $this->route = Uri::normalize(preg_replace('~[/\\\\](\d+-)~', '/', $this->relativePath));
         $this->id = basename($this->path);
         $this->slug = basename($this->route);
