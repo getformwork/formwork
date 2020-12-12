7a02f7e96194f3981a6b7bd133590889149aab65
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2020-12-12 19:54:33 +0100

    Allow leading zeros for decimal numbers in DurationInput

--- admin/assets/js/src/components/durationinput.js
+++ admin/assets/js/src/components/durationinput.js
@@ -136,8 +136,8 @@ export default function DurationInput(input, options) {
             }
             innerInputs[name] = innerInput;
             innerInput.addEventListener('input', function () {
-                if (this.value.length > 1) {
-                    this.value = this.value.replace(/^0+/, '');
+                while (this.value.charAt(0) === '0' && this.value.length > 1 && !this.value.charAt(1).match(/[.,]/)) {
+                    this.value = this.value.slice(1);
                 }
                 while (this.value > Utils.getMaxSafeInteger()) {
                     this.value = this.value.slice(0, -1);
