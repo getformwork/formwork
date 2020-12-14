83c3f9f14496bd64930cb001c2f1c9875678c41e
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2020-12-14 17:08:09 +0100

    Add `countWords`, `truncate`, `truncateWords`, `readingTime`, `markdown`, `date` and `datetime` helpers

--- formwork/helpers.php
+++ formwork/helpers.php
@@ -20,5 +20,40 @@ return [
     'slug' => [
         'Formwork\\Utils\\Str',
         'slug'
-    ]
+    ],
+    'countWords' => [
+        'Formwork\\Utils\\Text',
+        'countWords'
+    ],
+    'truncate' => [
+        'Formwork\\Utils\\Text',
+        'truncate'
+    ],
+    'truncateWords' => [
+        'Formwork\\Utils\\Text',
+        'truncateWords'
+    ],
+    'readingTime' => [
+        'Formwork\\Utils\\Text',
+        'readingTime'
+    ],
+    'markdown' => static function (string $markdown): string {
+        $currentPage = \Formwork\Core\Formwork::instance()->site()->currentPage();
+        return \Formwork\Parsers\Markdown::parse(
+            $markdown,
+            ['baseRoute' => $currentPage ? $currentPage->route() : '/']
+        );
+    },
+    'date' => static function (int $timestamp): string {
+        return date(
+            \Formwork\Core\Formwork::instance()->option('date.format'),
+            $timestamp
+        );
+    },
+    'datetime' => static function (int $timestamp): string {
+        return date(
+            \Formwork\Core\Formwork::instance()->option('date.format') . ' ' . \Formwork\Core\Formwork::instance()->option('date.hour_format'),
+            $timestamp
+        );
+    }
 ];
