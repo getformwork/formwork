7e1d7541c1691bc19e1be5d72ce004222c7632be
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2020-12-19 21:57:53 +0100

    Add `Translations::getCurrent()` and `Translations::setCurrent()`

--- formwork/src/Translations/Translations.php
+++ formwork/src/Translations/Translations.php
@@ -18,6 +18,13 @@ class Translations extends Collection
     protected $storage = [];
 
     /**
+     * Current translation
+     *
+     * @var Translation
+     */
+    protected $current;
+
+    /**
      * Load a translation file
      */
     public function load(string $path): void
@@ -70,6 +77,22 @@ class Translations extends Collection
         }
 
         return $this->storage[$code] = new Translation($code, $data);
+    }
+
+    /**
+     * Set current translation from language code
+     */
+    public function setCurrent(string $code): void
+    {
+        $this->current = $this->get($code, true);
+    }
+
+    /**
+     * Get current translation
+     */
+    public function getCurrent(): Translation
+    {
+        return $this->current ?? $this->getFallback();
     }
 
     /**
