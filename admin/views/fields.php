ff12f90951d136eaadba329e1d78ca250ec5be7f
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2021-05-12 00:17:08 +0200

    Add `fields` to admin views

---
+++ admin/views/fields.php
@@ -0,0 +1,5 @@
+<?php foreach ($fields as $field): ?>
+    <?php if ($field->isVisible()): ?>
+        <?php $this->insert('fields.' . $field->type(), ['field' => $field]) ?>
+    <?php endif; ?>
+<?php endforeach; ?>
