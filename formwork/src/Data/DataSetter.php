27c9474c3046bbe8871691435a7c808e7f7fa3e3
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2022-11-12 21:13:48 +0100

    Fix `DataSetter` still extending final `DataGetter`

--- formwork/src/Data/DataSetter.php
+++ formwork/src/Data/DataSetter.php
@@ -2,9 +2,37 @@
 
 namespace Formwork\Data;
 
+use Formwork\Data\Traits\DataArrayable;
+use Formwork\Data\Traits\DataMultipleGetter;
 use Formwork\Data\Traits\DataMultipleSetter;
 
-final class DataSetter extends DataGetter
+final class DataSetter
 {
+    use DataArrayable;
+    use DataMultipleGetter;
     use DataMultipleSetter;
+
+    /**
+     * Create a new instance
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Return whether data is present
+     */
+    public function isEmpty(): bool
+    {
+        return empty($this->data);
+    }
+
+    /**
+     * Create an instance from another getter
+     */
+    public static function fromGetter(DataGetter|DataSetter $getter): self
+    {
+        return new static($getter->toArray());
+    }
 }
