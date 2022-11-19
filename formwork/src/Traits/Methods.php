529b542092a735308e564e8af3ff7783e3db6e6c
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2022-11-19 22:37:50 +0100

    Add trait `Methods`

---
+++ formwork/src/Traits/Methods.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Formwork\Traits;
+
+use BadMethodCallException;
+
+trait Methods
+{
+    /**
+     * Array of methods
+     */
+    protected array $methods = [];
+
+    /**
+     * Return whether a method is defined in the `$method` property
+     */
+    public function hasMethod(string $method): bool
+    {
+        return isset($this->methods[$method]);
+    }
+
+    /**
+     * Call a method defined in the `$method` property
+     */
+    protected function callMethod(string $method, array $arguments)
+    {
+        return $this->methods[$method](...$arguments);
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        if ($this->hasMethod($name)) {
+            return $this->callMethod($name, $arguments);
+        }
+        throw new BadMethodCallException(sprintf('Call to undefined method %s::%s()', static::class, $name));
+    }
+}
