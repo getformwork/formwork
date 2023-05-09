b31ee38a5f105cfbe4d0afa6c3b96f574634374e
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2023-05-09 18:59:19 +0200

    Add ticks and progress to range input

--- panel/assets/js/src/components/forms/range.js
+++ panel/assets/js/src/components/forms/range.js
@@ -2,7 +2,41 @@ export default function RangeInput(input) {
     input.addEventListener('change', updateValueLabel);
     input.addEventListener('input', updateValueLabel);
 
+    updateValueLabel.call(input);
+
+    if (input.hasAttribute('data-ticks')) {
+        const count = input.getAttribute('data-ticks');
+
+        switch (count) {
+        case 0:
+            break;
+
+        case 'true':
+        case '':
+            addTicks((input.max - input.min) / (input.step || 1) + 1);
+            break;
+
+        default:
+            addTicks(parseInt(count) + 1);
+            break;
+        }
+    }
+
     function updateValueLabel() {
+        this.style.setProperty('--progress', Math.round(this.value / (this.max - this.min) * 100) + '%');
         $('output[for="' + this.id + '"]').innerHTML = this.value;
+    }
+
+    function addTicks(count) {
+        const ticks = document.createElement('div');
+        ticks.className = 'input-range-ticks';
+        ticks.setAttribute('data-for', input.id);
+        input.parentElement.insertBefore(ticks, input.nextSibling);
+
+        for (let i = 0; i < count; i++) {
+            const tick = document.createElement('div');
+            tick.className = 'tick';
+            ticks.appendChild(tick);
+        }
     }
 }
--- panel/views/fields/range.php
+++ panel/views/fields/range.php
@@ -1,16 +1,19 @@
 <?= $this->layout('fields.field') ?>
-<div>
-    <input <?= $this->attr([
-        'type'        => 'range',
-        'id'          => $field->name(),
-        'name'        => $field->formName(),
-        'min'         => $field->get('min'),
-        'max'         => $field->get('max'),
-        'step'        => $field->get('step'),
-        'value'       => $field->value(),
-        'required'    => $field->isRequired(),
-        'disabled'    => $field->isDisabled(),
-        'hidden'      => $field->isHidden()
-    ]) ?>>
+<div class="flex">
+    <div class="input-range">
+        <input <?= $this->attr([
+            'type'        => 'range',
+            'id'          => $field->name(),
+            'name'        => $field->formName(),
+            'min'         => $field->get('min'),
+            'max'         => $field->get('max'),
+            'step'        => $field->get('step'),
+            'value'       => $field->value(),
+            'required'    => $field->isRequired(),
+            'disabled'    => $field->isDisabled(),
+            'hidden'      => $field->isHidden(),
+            'data-ticks'  => $field->get('ticks')
+        ]) ?>>
+    </div>
     <output class="input-range-value" for="<?= $field->name() ?>"><?= $field->value() ?></output>
 </div>
