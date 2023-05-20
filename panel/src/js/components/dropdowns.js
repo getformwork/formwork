046ff0a21e0515b7e5f2989c93da4a2a11f582e1
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2023-05-20 18:18:12 +0200

    Fix dropdowns click event

--- panel/src/js/components/dropdowns.js
+++ panel/src/js/components/dropdowns.js
@@ -2,20 +2,21 @@ export default {
     init: function () {
         if ($('.dropdown')) {
             document.addEventListener('click', (event) => {
+                $$('.dropdown-menu').forEach((element) => {
+                    element.style.display = '';
+                });
+
                 const button = event.target.closest('.dropdown-button');
 
                 if (button) {
                     const dropdown = document.getElementById(button.getAttribute('data-dropdown'));
                     const isVisible = getComputedStyle(dropdown).display !== 'none';
                     event.preventDefault();
+
                     if (dropdown && !isVisible) {
                         dropdown.style.display = 'block';
                     }
                 }
-
-                $$('.dropdown-menu').forEach((element) => {
-                    element.style.display = '';
-                });
             });
         }
     },
