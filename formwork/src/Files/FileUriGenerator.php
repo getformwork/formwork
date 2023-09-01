70cfa184659fc9ee115bfcd642213cccec7b2230
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2023-09-02 01:08:32 +0200

    Normalize path in `FileUriGenerator`

--- formwork/src/Files/FileUriGenerator.php
+++ formwork/src/Files/FileUriGenerator.php
@@ -6,6 +6,7 @@ use Exception;
 use Formwork\Config;
 use Formwork\Pages\Site;
 use Formwork\Router\Router;
+use Formwork\Utils\FileSystem;
 use Formwork\Utils\Str;
 
 class FileUriGenerator
@@ -19,14 +20,14 @@ class FileUriGenerator
     {
         $path = $file->path();
 
-        if (Str::startsWith($path, $this->config->get('system.images.processPath'))) {
+        if (Str::startsWith($path, FileSystem::normalizePath($this->config->get('system.images.processPath')))) {
             $id = basename(dirname($path));
             $name = basename($path);
             $uriPath = $this->router->generate('assets', compact('id', 'name'));
             return $this->site->uri($uriPath, includeLanguage: false);
         }
 
-        if (Str::startsWith($path, $contentPath = $this->config->get('system.content.path'))) {
+        if (Str::startsWith($path, $contentPath = FileSystem::normalizePath($this->config->get('system.content.path')))) {
             $uriPath = Str::after($path, $contentPath);
             return $this->site->uri($uriPath, includeLanguage: false);
         }
