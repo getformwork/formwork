37c3218c0114d9e322f888eca9779ec799e05cca
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2023-12-03 15:08:58 +0100

    Update linting and integrate prettier

--- panel/eslint.config.js
+++ panel/eslint.config.js
@@ -1,61 +1,40 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import js from "@eslint/js";
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
     js.configs.recommended,
     {
-        'languageOptions': {
-            'ecmaVersion': 6,
-            'globals': {
+        languageOptions: {
+            ecmaVersion: 11,
+            globals: {
                 ...globals.browser,
-                '$': 'readonly',
-                '$$': 'readonly',
-                'Formwork': 'readonly',
+                $: "readonly",
+                $$: "readonly",
+                Formwork: "readonly",
             },
-            'sourceType': 'module',
+            sourceType: "module",
         },
-        'rules': {
-            'arrow-body-style': ['error', 'as-needed'],
-            'arrow-parens': ['error'],
-            'arrow-spacing': ['error'],
-            'brace-style': ['error', '1tbs'],
-            'comma-dangle': ['error', { 'objects': 'always-multiline' }],
-            'curly': ['error', 'all'],
-            'eqeqeq': ['error', 'always'],
-            'indent': ['error', 4],
-            'keyword-spacing': ['error'],
-            'linebreak-style': ['error', 'unix'],
-            'new-parens': ['error'],
-            'no-else-return': ['error'],
-            'no-lonely-if': ['error'],
-            'no-multi-spaces': ['error'],
-            'no-multiple-empty-lines': ['error', { 'max': 1 }],
-            'no-trailing-spaces': ['error'],
-            'no-unneeded-ternary': ['error'],
-            'no-var': ['error'],
-            'object-curly-newline': ['error', { 'multiline': true }],
-            'object-curly-spacing': ['error', 'always'],
-            'object-property-newline': ['error'],
-            'one-var': ['error', {
-                'initialized': 'never',
-                'uninitialized': 'always',
-            }],
-            'operator-assignment': ['error'],
-            'prefer-arrow-callback': ['error'],
-            'prefer-const': ['error'],
-            'prefer-template': ['error'],
-            'quotes': ['error', 'single'],
-            'space-before-function-paren': ['error', {
-                'anonymous': 'always',
-                'named': 'never',
-            }],
-            'space-infix-ops': ['error'],
-            'space-unary-ops': ['error', {
-                'nonwords': false,
-                'words': true,
-            }],
-            'spaced-comment': ['error'],
-            'template-curly-spacing': ['error', 'never'],
+        rules: {
+            "arrow-body-style": ["error", "as-needed"],
+            curly: ["error", "all"],
+            eqeqeq: ["error", "always"],
+            "no-else-return": ["error"],
+            "no-lonely-if": ["error"],
+            "no-unneeded-ternary": ["error"],
+            "no-var": ["error"],
+            "one-var": [
+                "error",
+                {
+                    initialized: "never",
+                    uninitialized: "always",
+                },
+            ],
+            "operator-assignment": ["error"],
+            "prefer-arrow-callback": ["error"],
+            "prefer-const": ["error"],
+            "prefer-template": ["error"],
         },
-    }
+    },
+    eslintConfigPrettier,
 ];
