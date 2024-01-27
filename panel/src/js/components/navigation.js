4d5d6ca3ebab58a8731641b7386e2831102b5ee7
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-01-27 14:12:54 +0100

    Add sidebar backdrop

--- panel/src/js/components/navigation.js
+++ panel/src/js/components/navigation.js
@@ -3,7 +3,20 @@ import { $ } from "../utils/selectors";
 export class Navigation {
     constructor() {
         if ($(".toggle-navigation")) {
-            $(".toggle-navigation").addEventListener("click", () => $(".sidebar").classList.toggle("show"));
+            $(".toggle-navigation").addEventListener("click", () => {
+                if ($(".sidebar").classList.toggle("show")) {
+                    if (!$(".sidebar-backdrop")) {
+                        const backdrop = document.createElement("div");
+                        backdrop.className = "sidebar-backdrop hide-from-s";
+                        document.body.appendChild(backdrop);
+                    }
+                } else {
+                    const backdrop = $(".sidebar-backdrop");
+                    if (backdrop) {
+                        backdrop.parentNode.removeChild(backdrop);
+                    }
+                }
+            });
         }
 
         if ($("[data-command=save]")) {
