143f60778a4f819c4f4a79acdec1dbc02459ddb1
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-05-01 15:35:19 +0200

    Make field and modal labels uniform

--- panel/views/fields/partials/label.php
+++ panel/views/fields/partials/label.php
@@ -1,3 +1,3 @@
 <?php if ($field->has('label')) : ?>
-    <label class="form-label" for="<?= $field->name() ?>"><?= $field->label() ?></label>
+    <label class="<?= $this->classes(['form-label', 'form-label-required' => $field->isRequired()]) ?>" for="<?= $field->name() ?>"><?= $this->append($field->label(), ':') ?></label>
 <?php endif ?>
