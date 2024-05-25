6adc302f5a294f2ffbbf1571dd4ffea6b7876723
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-05-25 14:34:25 +0200

    Add `content.safeMode` (enabled by default) to avoid XSS vulnerabilities

--- formwork/src/Parsers/Markdown.php
+++ formwork/src/Parsers/Markdown.php
@@ -2,9 +2,11 @@
 
 namespace Formwork\Parsers;
 
+use Formwork\App;
 use Formwork\Parsers\Extensions\CommonMark\LinkBaseExtension;
 use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\DisallowedRawHtml\DisallowedRawHtmlExtension;
 use League\CommonMark\Extension\Table\TableExtension;
 use League\CommonMark\MarkdownConverter;
 
@@ -17,11 +19,19 @@ class Markdown extends AbstractParser
      */
     public static function parse(string $input, array $options = []): string
     {
-        $environment = new Environment(['formwork' => $options]);
+        $safeMode = App::instance()->config()->get('system.content.safeMode', true);
+
+        $environment = new Environment([
+            'html_input'         => $safeMode ? 'escape' : 'allow',
+            'allow_unsafe_links' => false,
+            'max_nesting_level'  => 10,
+            'formwork'           => $options,
+        ]);
 
         $environment->addExtension(new CommonMarkCoreExtension());
         $environment->addExtension(new TableExtension());
         $environment->addExtension(new LinkBaseExtension());
+        $environment->addExtension(new DisallowedRawHtmlExtension());
 
         $markdownConverter = new MarkdownConverter($environment);
 
