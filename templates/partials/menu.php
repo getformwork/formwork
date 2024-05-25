e8a8140efd86f698c98f3fcc6bf0d16e506c8570
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-05-25 11:11:44 +0200

    Escape titles and tags

--- templates/partials/menu.php
+++ templates/partials/menu.php
@@ -1,11 +1,11 @@
 <nav class="menu">
     <div class="container">
-        <a class="menu-header" href="<?= $site->uri() ?>"><?= $site->title() ?></a>
+        <a class="menu-header" href="<?= $site->uri() ?>"><?= $this->escape($site->title()) ?></a>
         <button type="button" class="menu-toggle" data-toggle="main-menu" aria-expanded="false">&#9776;</button>
         <div class="menu-list menu-collapse" id="main-menu">
-        <?php foreach ($site->children()->filter('visible') as $item): ?>
-            <a class="menu-item<?php if ($item->isCurrent()): ?> active<?php endif; ?>" href="<?= $item->uri() ?>"><?= $item->get('menu', $item->title()) ?></a>
-        <?php endforeach; ?>
+            <?php foreach ($site->children()->filter('visible') as $item) : ?>
+                <a class="menu-item<?php if ($item->isCurrent()) : ?> active<?php endif; ?>" href="<?= $item->uri() ?>"><?= $this->escape($item->get('menu', $item->title())) ?></a>
+            <?php endforeach; ?>
         </div>
     </div>
 </nav>
