f5312015a5a5e89b95ef2bd07e496f8474d579c5
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-06-07 11:30:16 +0200

    Escape meta attributes to avoid XSS injection

--- site/templates/partials/meta.php
+++ site/templates/partials/meta.php
@@ -1,9 +1,9 @@
 <?php foreach ($page->metadata() as $meta) : ?>
     <?php if ($meta->isCharset()) : ?>
-        <meta charset="<?= $meta->content() ?>">
+        <meta charset="<?= $this->escapeAttr($meta->content()) ?>">
     <?php elseif ($meta->isHTTPEquiv()) : ?>
-        <meta http-equiv="<?= $meta->name() ?>" content="<?= $meta->content() ?>">
+        <meta http-equiv="<?= $this->escapeAttr($meta->name()) ?>" content="<?= $this->escapeAttr($meta->content()) ?>">
     <?php else : ?>
-        <meta <?= $meta->prefix() === 'og' ? 'property' : 'name' ?>="<?= $meta->name() ?>" content="<?= $meta->content() ?>">
+        <meta <?= $meta->prefix() === 'og' ? 'property' : 'name' ?>="<?= $this->escapeAttr($meta->name()) ?>" content="<?= $this->escapeAttr($meta->content()) ?>">
     <?php endif ?>
 <?php endforeach ?>
