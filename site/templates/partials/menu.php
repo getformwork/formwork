f522b338099a2ee5428dc181b450240288c06f91
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-06-15 17:59:39 +0200

    Fix always visible menu button in default template layout

--- site/templates/partials/menu.php
+++ site/templates/partials/menu.php
@@ -1,7 +1,7 @@
 <nav class="menu">
     <div class="container">
         <a class="menu-header" href="<?= $site->uri() ?>"><?= $this->escape($site->title()) ?></a>
-        <button type="button" class="button menu-toggle" data-toggle="main-menu" aria-expanded="false">&#9776;</button>
+        <button type="button" class="menu-toggle" data-toggle="main-menu" aria-expanded="false">&#9776;</button>
         <div class="menu-list menu-collapse" id="main-menu">
             <?php foreach ($site->children()->published()->listed() as $item) : ?>
                 <a class="<?= $this->classes(['menu-item', 'active' => $item->isCurrent()]) ?>" href="<?= $item->uri() ?>"><?= $this->escape($item->get('menu', $item->title())) ?></a>
