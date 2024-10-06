eac982350e216d82b00d92bd0b1449ed54d61cd2
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-10-06 18:16:35 +0200

    Add scheme and title reference to `Template`

--- formwork/src/Templates/TemplateFactory.php
+++ formwork/src/Templates/TemplateFactory.php
@@ -2,16 +2,20 @@
 
 namespace Formwork\Templates;
 
+use Formwork\Schemes\Schemes;
 use Formwork\Services\Container;
 
 class TemplateFactory
 {
-    public function __construct(protected Container $container)
+    public function __construct(protected Container $container, protected Schemes $schemes)
     {
     }
 
     public function make(string $name): Template
     {
-        return $this->container->build(Template::class, compact('name'));
+        return $this->container->build(Template::class, [
+            'name'   => $name,
+            'scheme' => $this->schemes->get('pages.' . $name),
+        ]);
     }
 }
