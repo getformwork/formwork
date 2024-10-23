4066211cd7c1339ae277cce955001f50ba399f57
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2024-10-23 23:56:13 +0200

    Add `AbstractController::forward()`

--- formwork/src/Controllers/AbstractController.php
+++ formwork/src/Controllers/AbstractController.php
@@ -5,8 +5,11 @@ namespace Formwork\Controllers;
 use Formwork\App;
 use Formwork\Config\Config;
 use Formwork\Http\Request;
+use Formwork\Http\Response;
+use Formwork\Services\Container;
 use Formwork\Utils\Str;
 use Formwork\View\ViewFactory;
+use InvalidArgumentException;
 
 abstract class AbstractController
 {
@@ -16,6 +19,7 @@ abstract class AbstractController
     protected string $name;
 
     public function __construct(
+        private readonly Container $container,
         protected App $app,
         protected Config $config,
         protected ViewFactory $viewFactory,
@@ -32,5 +36,20 @@ abstract class AbstractController
     protected function view(string $name, array $data = []): string
     {
         return $this->viewFactory->make($name, $data)->render();
+    }
+
+    /**
+     * Forward the request to another controller
+     *
+     * @param class-string<self>   $controller
+     * @param array<string, mixed> $parameters
+     */
+    protected function forward(string $controller, string $action, array $parameters = []): Response
+    {
+        if (!is_subclass_of($controller, AbstractController::class)) {
+            throw new InvalidArgumentException(sprintf('Controllers must extend %s', AbstractController::class));
+        }
+        $instance = $this->container->build($controller);
+        return $this->container->call($instance->$action(...), $parameters);
     }
 }
