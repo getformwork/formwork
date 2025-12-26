c04762c161afb25bc0dc94f7d6b348b4673b9841
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-12-26 23:33:24 +0100

    Improve dark color scheme styles

--- panel/views/layouts/panel.php
+++ panel/views/layouts/panel.php
@@ -20,11 +20,11 @@
         <span class="show-from-sm text-color-gray-dark"><?= $this->translate('panel.panel') ?></span>
         <span class="show-from-sm ml-5 mr-2 text-color-gray-medium">/</span>
         <?php if ($panel->user()->permissions()->has('panel.options.site')) : ?>
-            <span class="flex-grow-1"><a class="button button-link text-size-md" href="<?= $panel->uri('/options/site/') ?>"><?= $this->icon('globe') ?> <span class="ml-2"><?= $this->escape($site->title()) ?></span></a></span>
+            <span class="flex-grow-1"><a class="button button-link" href="<?= $panel->uri('/options/site/') ?>"><?= $this->icon('globe') ?> <span class="ml-2"><?= $this->escape($site->title()) ?></span></a></span>
         <?php else : ?>
             <span class="flex-grow-1"><span class="pl-4"><?= $this->icon('globe') ?> <span class="ml-2"><?= $this->escape($site->title()) ?></span></span></span>
         <?php endif ?>
-        <a href="<?= $site->uri() ?>" class="button button-link text-size-md" target="formwork-view-site"><span class="show-from-xs"><?= $this->translate('panel.viewSite') ?></span> <?= $this->icon('arrow-right-up-box') ?></a>
+        <a href="<?= $site->uri() ?>" class="button button-link" target="formwork-view-site"><span class="show-from-xs"><?= $this->translate('panel.viewSite') ?></span> <?= $this->icon('arrow-right-up-box') ?></a>
     </header>
     <main class="panel-main">
         <div class="container">
