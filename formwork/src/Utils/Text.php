77941bc020f3bc59f25ab41af40883dba47b72ac
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-12-30 12:12:24 +0100

    Remove leading and trailing whitespace with `Text::normalizeWhitespace()`

--- formwork/src/Utils/Text.php
+++ formwork/src/Utils/Text.php
@@ -36,8 +36,9 @@ final class Text
      */
     public static function normalizeWhitespace(string $text): string
     {
-        return preg_replace(self::WHITESPACE_REGEX, self::WHITESPACE_SEQUENCE, $text)
+        $normalized = preg_replace(self::WHITESPACE_REGEX, self::WHITESPACE_SEQUENCE, $text)
             ?? throw new RuntimeException(sprintf('Whitespace replacement failed with error: %s', preg_last_error_msg()));
+        return trim($normalized);
     }
 
     /**
