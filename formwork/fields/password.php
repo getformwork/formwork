5a9297e8e7ef90925909480ef8cb9fb04019072a
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2026-02-16 20:48:09 +0100

    Add the possibility to ignore empty values in password fields

--- formwork/fields/password.php
+++ formwork/fields/password.php
@@ -1,9 +1,24 @@
 <?php
 
 use Formwork\Cms\App;
+use Formwork\Fields\Field;
+use Formwork\Utils\Constraint;
 
 return function (App $app) {
     return [
         'extend' => 'text',
+
+        'methods' => [
+            'ignoreEmpty' => function (Field $field) {
+                return $field->is('ignoreEmpty', false);
+            },
+
+            'setValue' => function (Field $field, $value) {
+                if ($field->ignoreEmpty() && Constraint::isEmpty($value)) {
+                    return $field->value();
+                }
+                return $value;
+            },
+        ],
     ];
 };
