b34cb46b73779641e8a4c4a850e8a277ba18fd9e
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2026-02-28 12:34:43 +0100

    Add tooltip to page status in the editor view

--- panel/views/partials/pages/status.php
+++ panel/views/partials/pages/status.php
@@ -1,5 +1,5 @@
 <?php if ($page->status() === 'published') : ?>
-    <span class="page-status-published mr-2"><?= $this->icon('circle-small-fill') ?></span>
+    <span class="page-status-published mr-2" title="<?= $this->translate("page.status.{$page->status()}") ?>"><?= $this->icon('circle-small-fill') ?></span>
 <?php elseif ($page->status() === 'notPublished') : ?>
-    <span class="page-status-not-published mr-2"><?= $this->icon('circle-small-fill') ?></span>
+    <span class="page-status-not-published mr-2" title="<?= $this->translate("page.status.{$page->status()}") ?>"><?= $this->icon('circle-small-fill') ?></span>
 <?php endif ?>
