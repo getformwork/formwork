ee1bbf2d5940c0a452e72f97872dd5c8fcd18214
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2026-08-16 11:51:17 +0200

    Restore `ConfigServiceLoader::clearCache()`

--- formwork/src/Services/Loaders/ConfigServiceLoader.php
+++ formwork/src/Services/Loaders/ConfigServiceLoader.php
@@ -55,6 +55,17 @@ final class ConfigServiceLoader implements ServiceLoaderInterface
         return $config;
     }
 
+    /**
+     * Clear config cache
+     *
+     * @internal
+     */
+    public static function clearCache(): void
+    {
+        FileSystem::delete(self::CACHE_PATH, recursive: true);
+        FileSystem::createDirectory(self::CACHE_PATH, recursive: true);
+    }
+
     private function getConfigCacheFile(): ?string
     {
         try {
@@ -63,11 +74,10 @@ final class ConfigServiceLoader implements ServiceLoaderInterface
             return null;
         }
 
-        $cachePath = ROOT_PATH . '/cache/config/';
-        $cacheFile = FileSystem::joinPaths($cachePath, "config.{$host}.php");
+        $cacheFile = FileSystem::joinPaths(self::CACHE_PATH, "config.{$host}.php");
 
-        if (!FileSystem::isDirectory($cachePath, assertExists: false)) {
-            FileSystem::createDirectory($cachePath, recursive: true);
+        if (!FileSystem::isDirectory(self::CACHE_PATH, assertExists: false)) {
+            FileSystem::createDirectory(self::CACHE_PATH, recursive: true);
         }
 
         return $cacheFile;
