2774593d033b6df1861c64aafef297696ef4f6c6
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2026-08-26 14:38:42 +0200

    Reserve "plugin" used as plugin id

--- formwork/src/Plugins/Plugin.php
+++ formwork/src/Plugins/Plugin.php
@@ -42,6 +42,10 @@ class Plugin implements Arrayable
     ) {
         $this->id = basename($this->path);
 
+        if ($this->id === 'plugin') {
+            throw new InvalidArgumentException('Invalid plugin id "plugin". The plugin id "plugin" is reserved.');
+        }
+
         if (!preg_match('/^[a-z0-9-]+$/', $this->id)) {
             throw new InvalidArgumentException(sprintf('Invalid plugin id "%s". Plugin ids can only contain lowercase letters, numbers and hyphens.', $this->id));
         }
