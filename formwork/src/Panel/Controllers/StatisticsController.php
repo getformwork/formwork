4d5980792f1da615d30758decf5a400199ec77c4
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2026-09-06 23:47:17 +0200

    Add the possibility to download statistics data as TSV or CSV

--- formwork/src/Panel/Controllers/StatisticsController.php
+++ formwork/src/Panel/Controllers/StatisticsController.php
@@ -2,9 +2,16 @@
 
 namespace Formwork\Panel\Controllers;
 
+use Formwork\Http\FileResponse;
 use Formwork\Http\Response;
+use Formwork\Parsers\Csv;
 use Formwork\Parsers\Json;
+use Formwork\Router\RouteParams;
 use Formwork\Statistics\Statistics;
+use Formwork\Utils\Arr;
+use Formwork\Utils\FileSystem;
+use Formwork\Utils\Str;
+use ZipArchive;
 
 final class StatisticsController extends AbstractController
 {
@@ -35,5 +42,55 @@ final class StatisticsController extends AbstractController
             'monthUniqueVisits' => array_sum($statistics->getUniqueVisits(31)),
             'weekUniqueVisits'  => array_sum($statistics->getUniqueVisits(7)),
         ]));
+    }
+
+    /**
+     * Statistics@download action
+     */
+    public function download(Statistics $statistics, RouteParams $routeParams): Response
+    {
+        if (!$this->hasPermission('panel.statistics.download')) {
+            return $this->forward(ErrorsController::class, 'forbidden');
+        }
+
+        $format = $routeParams->get('format', 'tsv');
+
+        $chartData = $statistics->getChartData(31, 'Y-m-d');
+
+        $stats = [
+            'devices' => [
+                ['Device', 'Count'],
+                ...Arr::entries($statistics->getDevices()),
+            ],
+            'pageViews' => [
+                ['Page', 'Count'],
+                ...Arr::entries($statistics->getPageViews()),
+            ],
+            'sources' => [
+                ['Source', 'Count'],
+                ...Arr::entries($statistics->getSources()),
+            ],
+            'visits' => [
+                ['Date', 'Visits', 'UniqueVisits'],
+                ...Arr::zip([$chartData['labels'], ...$chartData['series']]),
+            ],
+        ];
+
+        $file = FileSystem::joinPaths($this->config->getString('site.statistics.path'), sprintf('.export-%s.zip', FileSystem::randomName()));
+
+        $zipArchive = new ZipArchive();
+        $zipArchive->open($file, ZipArchive::CREATE);
+
+        foreach ($stats as $name => $data) {
+            $zipArchive->addFromString(
+                sprintf('%s.%s', $name, $format),
+                Csv::encode($data, ['separator' => $format === 'tsv' ? Csv::SEPARATOR_TAB : Csv::SEPARATOR_COMMA])
+            );
+        }
+
+        $zipArchive->close();
+
+        return (new FileResponse($file, download: true, deleteAfterSend: true))
+            ->setFilename(sprintf('statistics-%s-%s.zip', Str::slug($this->site->title()), date('Ymd-His')));
     }
 }
