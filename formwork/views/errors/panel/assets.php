a655227a175d51002297aba3dca01108b478fde4
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2026-09-06 18:42:55 +0200

    Fix bootsrap error views requiring unavailable view methods

--- formwork/views/errors/panel/assets.php
+++ formwork/views/errors/panel/assets.php
@@ -1,4 +1,4 @@
-<?php include dirname(__DIR__) . '/partials/header.php' ?>
+<?php $this->insert('@system.errors.partials.header') ?>
 <h2>The administration panel is currently offline due to technical problems</h2>
 <p>Required panel assets were not found. If you are the maintainer of this site, please run <code>cd panel; pnpm install && pnpm build</code></p>
-<?php include dirname(__DIR__) . '/partials/footer.php' ?>
+<?php $this->insert('@system.errors.partials.footer') ?>
