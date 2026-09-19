dd6c25153edb543be41588bc0b398db17b3b55e2
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2026-09-20 00:39:12 +0200

    Refactor fields filtering in `PluginsController` to use `filterBy()`

--- formwork/src/Panel/Controllers/PluginsController.php
+++ formwork/src/Panel/Controllers/PluginsController.php
@@ -75,7 +75,7 @@ final class PluginsController extends AbstractController
         return new Response($this->view('@panel.plugins.plugin', [
             'title'  => $plugin->manifest()->title() ?? $plugin->name(),
             'plugin' => $plugin,
-            'fields' => $form->fields()->reject(fn($field) => $field->name() === 'enabled'),
+            'fields' => $form->fields()->filterBy('name', '!==', 'enabled'),
             ...$this->getPreviousAndNextPlugin($plugin),
         ]), $form->getResponseStatus());
     }
